775be306ab285f9d75d7fa26486cfb96804e2390
MarcusHaigler <haiglerm41@gmail.com>
2024-06-04 13:18:39 -0400

    bank_demo.py

---
+++ bank_demo.py
@@ -0,0 +1,82 @@
+'''
+banking application
+this version is done in the CLI for simplicity
+and to focus on the functionality of the app
+'''
+
+import os
+
+balance = 100
+running = True
+
+def show_balance():
+    os.system('cls||clear')
+
+    print(f"Current Balance: ${balance}")
+    user_input = input("""Options:
+1) withdraw
+2) deposit
+3) back to home
+""")
+    
+    if user_input == '1':
+        withdraw()
+    elif user_input == '2':
+        deposit()
+    elif user_input == '3':
+        home_menu()
+
+def deposit():
+    os.system('cls||clear')
+
+    global balance
+
+    deposit_amount = int(input("enter amount to deposit: "))
+    balance += deposit_amount
+    print(f"Deposit succesful, new balance: ${balance}")
+    home_menu()
+
+def withdraw():
+    os.system('cls||clear')
+
+    global balance
+    withdrawal_amount = int(input("enter amount to withdraw: "))
+    if withdrawal_amount > balance:
+        print("insufficient funds")
+        home_menu()
+    elif withdrawal_amount <= balance:
+        balance -= withdrawal_amount
+        print(f"Successful withdrawal, new balance: ${balance}")
+    home_menu()
+
+def home_menu():
+
+    global running
+
+    options_prompt = """## Sample Bank Demo ## 
+enter one of the following options:
+1) show balance
+2) withdraw
+3) deposit
+4) exit
+"""
+
+    user_input = input(options_prompt)
+
+    if user_input == '1':
+        show_balance()
+        print(options_prompt)
+    elif user_input == '2':
+        withdraw()
+    elif user_input == '3':
+        deposit()
+    elif user_input == '4':
+        print("program ended, goodbye")
+        running = False
+    else:
+        os.system('cls||clear')
+        print("invalid option, try again")
+        home_menu()   
+
+while running == True: 
+    home_menu()
